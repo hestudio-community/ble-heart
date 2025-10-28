3932a6af34f76a2ab38a47ec84e5ed49c5d2152e
undefined <hestudio@hestudio.net>
2025-10-28 08:53:49 +0800

    Initial Commit

---
+++ BLE Heart/BLE_HeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLE_HeartApp.swift
+//  BLE Heart
+//
+//  Created by undefined on 2025/10/28.
+//
+
+import SwiftUI
+
+@main
+struct BLE_HeartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLE Heart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BLE Heart
+//
+//  Created by undefined on 2025/10/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
